5cd856aa654354d96801f838f432d40101b85c13
Mahii <90246070+mahiiverse1@users.noreply.github.com>
2022-02-02 17:59:52 +0530

    Create script.js

---
+++ tasks/task1/script.js
@@ -0,0 +1,7 @@
+function login() {
+    var Details = new Array();
+    Details[0] = document.getElementById("email").value;
+    Details[1] = document.getElementById("password").value;
+    console.log(Details);
+    alert("You have logged in!!");
+}
